4b2c038f52cab9b9ae3f51453abe679f28f11005
coldjacketg <153304881+coldjacketg@users.noreply.github.com>
2023-12-08 10:57:16 -0500

    Add files via upload

--- HolidayTreeLights.ino
+++ HolidayTreeLights.ino
@@ -28,7 +28,7 @@
 #define COLOR_BLUE    4
 #define COLOR_PURPLE  5
 
-#define MODES 5  // don't like candle in this version, so skip it
+#define MODES 6
 #define MODE_FADE 0
 #define MODE_SPARKLE 1
 #define MODE_GLITTER 2
@@ -40,8 +40,12 @@
 CRGB leds[NUM_LEDS];
 
 int lightLed[NUM_LIGHTS];
-CRGB lightRGB[NUM_LIGHTS];
 byte lightDelay[NUM_LIGHTS];
+CRGB lightData[NUM_LIGHTS];
+// lightData (3 bytes) is used to store RGB color to restore after strobing in MODE_SPARKLE,
+// lightdata.b for brightness in MODE_FADE for fading in,
+// lightData.g for palette hue in MODE_CANDLE,
+// lightData[0]/lightData[1] for two RGB colors in MODE_GRADIENT with moving stripes
 
 CRGBPalette16 candlePalette;
 
@@ -114,6 +118,8 @@ void getNewLight(byte light)
         notFound = true;
   }
   lightDelay[light] = random(50, 256);
+  lightData[light].r = 0;         // used for MODE_FADE as a flag for new hue (lightData[light].g is used for hue)
+  lightData[light].b = 0;         // used for MODE_FADE for fade-in brightness
 }
 
 void setLeds()
@@ -124,8 +130,13 @@ void setLeds()
 
 void setLed(int led)
 {
+  setLed(led, getHue(), getSaturation(), 255);
+}
+
+void setLed(int led, byte hue, byte saturation, byte brightness)
+{
   if (((mode != MODE_GLITTER) && (mode != MODE_GLITTER_WAVE)) || (random(100) < glitterChance))
-    leds[led] = CHSV(getHue(), getSaturation(), 255);
+    leds[led] = CHSV(hue, saturation, brightness);
 }
 
 boolean isLit(int led)
@@ -149,10 +160,13 @@ boolean isWhite(int led)
 
 void setCandle(byte light)
 {
-  byte paletteIndex = lightRGB[light].r;  // use RGB red channel for candle palette hue
-  paletteIndex = paletteIndex + random(50);
-  lightRGB[light].r = paletteIndex;
-  leds[lightLed[light]] = ColorFromPalette(candlePalette, paletteIndex);
+  byte paletteIndex = lightData[light].r;  // use RGB red channel for candle palette hue
+  paletteIndex = paletteIndex + random(30);  // gradual changes in color from the palette 
+  lightData[light].r = paletteIndex;
+  byte brightness = 255;
+  if (random(10) == 0)  // 1/10 chance to flicker by a random amount
+    brightness = random(200,255);
+  leds[lightLed[light]] = ColorFromPalette(candlePalette, paletteIndex, brightness);
 }
 
 
@@ -169,27 +183,45 @@ void handleLight(byte light)
     switch (mode)
     {
       case MODE_FADE:
+        if ((!lightData[light].r) && (isLit(lightLed[light])))  // haven't picked a new color yet and light is lit, so fade-out
+          leds[lightLed[light]].fadeToBlackBy(fadeRate);
+        else
+        {
+          if (!lightData[light].r)  // haven't picked a new color yet, so pick a new color
+          {
+            lightData[light].r = 1;
+            lightData[light].g = getHue();
+          }
+          int brightness = lightData[light].b;  // set brightness for fade-in
+          setLed(lightLed[light], lightData[light].g, getSaturation(), brightness);
+          if (brightness < 255)
+          {
+            brightness = brightness + fadeRate;
+            if (brightness > 255)
+              brightness = 255;
+            lightData[light].b = brightness;
+          }
+          else
+            getNewLight(light);
+        }
+        break;
       case MODE_CANDLE:
         if (isLit(lightLed[light]))
           leds[lightLed[light]].fadeToBlackBy(fadeRate);
         else
-        {
-          if (mode != MODE_CANDLE)
-            setLed(lightLed[light]);
           getNewLight(light);
-        }
         break;
       case MODE_SPARKLE:
         if (isWhite(lightLed[light]))
         {
-          leds[lightLed[light]] = lightRGB[light];
+          leds[lightLed[light]] = lightData[light];
           getNewLight(light);
         }
         else
         {
           if (random(3) == 0)  // only 1/3 of lights will trigger a strobe
           {
-            lightRGB[light] = leds[lightLed[light]];
+            lightData[light] = leds[lightLed[light]];
             leds[lightLed[light]] = CRGB(255, 255, 255);
           }
           else
@@ -254,33 +286,33 @@ void handleMode()
               break;
             case 1:  // red/white
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CHSV(HSVHue::HUE_RED, 255, 255);
-              lightRGB[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
+              lightData[0] = CHSV(HSVHue::HUE_RED, 255, 255);
+              lightData[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
               break;
             case 2:  // red/green
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CHSV(HSVHue::HUE_RED, 255, 255);
-              lightRGB[1] = CHSV(HSVHue::HUE_GREEN, 255, 255);
+              lightData[0] = CHSV(HSVHue::HUE_RED, 255, 255);
+              lightData[1] = CHSV(HSVHue::HUE_GREEN, 255, 255);
               break;
             case 3:  // blue/white
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CHSV(HSVHue::HUE_BLUE, 255, 255);
-              lightRGB[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
+              lightData[0] = CHSV(HSVHue::HUE_BLUE, 255, 255);
+              lightData[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
               break;
             case 4:  // green/white
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CHSV(HSVHue::HUE_GREEN, 255, 255);
-              lightRGB[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
+              lightData[0] = CHSV(HSVHue::HUE_GREEN, 255, 255);
+              lightData[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
               break;
             case 5:  // black/white
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CRGB(0, 0, 0);
-              lightRGB[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
+              lightData[0] = CRGB(0, 0, 0);
+              lightData[1] = CHSV(WARM_WHITE_HUE, WARM_WHITE_SATURATION, 255);
               break;
             case 6:  // orange/purple
               color = COLOR_MODE_WHITE;
-              lightRGB[0] = CHSV(HSVHue::HUE_ORANGE, 255, 255);
-              lightRGB[1] = CHSV(HSVHue::HUE_PURPLE, 255, 255);
+              lightData[0] = CHSV(HSVHue::HUE_ORANGE, 255, 255);
+              lightData[1] = CHSV(HSVHue::HUE_PURPLE, 255, 255);
               break;
           }
           gradientStart = 0;
@@ -343,9 +375,9 @@ void handleMode()
             if (bandEnd >= NUM_LEDS)
               bandEnd = NUM_LEDS - 1;
             if (band%2 == 0)
-              fill_solid(&leds[bandStart], bandEnd - bandStart, lightRGB[0]);
+              fill_solid(&leds[bandStart], bandEnd - bandStart, lightData[0]);
             else
-              fill_solid(&leds[bandStart], bandEnd - bandStart, lightRGB[1]);
+              fill_solid(&leds[bandStart], bandEnd - bandStart, lightData[1]);
           }
           gradientStart = gradientStart + 3;
           if (gradientStart >= (NUM_BAND_SIZE*2))  // must shift two full bands to both colors
@@ -399,85 +431,22 @@ void serialInfo()
   Serial.print(lightLed[0]);
   Serial.print(", lightDelay[0]=");
   Serial.print(lightDelay[0]);
-  Serial.print(", lightRGB[0]=");
-  Serial.print(lightRGB[0].r);
+  Serial.print(", lightData[0]=");
+  Serial.print(lightData[0].r);
   Serial.print("/");
-  Serial.print(lightRGB[0].g);
+  Serial.print(lightData[0].g);
   Serial.print("/");
-  Serial.print(lightRGB[0].b);
+  Serial.print(lightData[0].b);
   Serial.print(", lightLed[1]=");
   Serial.print(lightLed[1]);
   Serial.print(", lightDelay[1]=");
   Serial.print(lightDelay[1]);
-  Serial.print(", lightRGB[1]=");
-  Serial.print(lightRGB[1].r);
+  Serial.print(", lightData[1]=");
+  Serial.print(lightData[1].r);
   Serial.print("/");
-  Serial.print(lightRGB[1].g);
+  Serial.print(lightData[1].g);
   Serial.print("/");
-  Serial.print(lightRGB[1].b);
-  Serial.print(", lightLed[2]=");
-  Serial.print(lightLed[2]);
-  Serial.print(", lightDelay[2]=");
-  Serial.print(lightDelay[2]);
-  Serial.print(", lightRGB[2]=");
-  Serial.print(lightRGB[2].r);
-  Serial.print("/");
-  Serial.print(lightRGB[2].g);
-  Serial.print("/");
-  Serial.print(lightRGB[2].b);
-  Serial.print(", lightLed[3]=");
-  Serial.print(lightLed[3]);
-  Serial.print(", lightDelay[3]=");
-  Serial.print(lightDelay[3]);
-  Serial.print(", lightRGB[2]=");
-  Serial.print(lightRGB[3].r);
-  Serial.print("/");
-  Serial.print(lightRGB[3].g);
-  Serial.print("/");
-  
-  Serial.print(lightRGB[3].b);
-  Serial.print(", lightLed[4]=");
-  Serial.print(lightLed[4]);
-  Serial.print(", lightDelay[4]=");
-  Serial.print(lightDelay[4]);
-  Serial.print(", lightRGB[4]=");
-  Serial.print(lightRGB[4].r);
-  Serial.print("/");
-  Serial.print(lightRGB[4].g);
-  Serial.print("/");
-  Serial.print(lightRGB[4].b);
-  Serial.print(", gradientStart=");
-  Serial.print(gradientStart);
-  Serial.print(", leds[0]=");
-  Serial.print(leds[0].r);
-  Serial.print("/");
-  Serial.print(leds[0].g);
-  Serial.print("/");
-  Serial.print(leds[0].b);
-  Serial.print(", leds[1]=");
-  Serial.print(leds[1].r);
-  Serial.print("/");
-  Serial.print(leds[1].g);
-  Serial.print("/");
-  Serial.print(leds[1].b);
-  Serial.print(", leds[2]=");
-  Serial.print(leds[2].r);
-  Serial.print("/");
-  Serial.print(leds[2].g);
-  Serial.print("/");
-  Serial.print(leds[2].b);
-  Serial.print(", leds[3]=");
-  Serial.print(leds[3].r);
-  Serial.print("/");
-  Serial.print(leds[3].g);
-  Serial.print("/");
-  Serial.print(leds[3].b);
-  Serial.print(", leds[4]=");
-  Serial.print(leds[4].r);
-  Serial.print("/");
-  Serial.print(leds[4].g);
-  Serial.print("/");
-  Serial.print(leds[4].b);
+  Serial.print(lightData[1].b);
 }
 
 void setup()
@@ -485,9 +454,21 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("\nresetting");
-  randomSeed(analogRead(0));
+  int seed=analogRead(0) * analogRead(1) * analogRead(2);
+  Serial.print("  seed=");
+  Serial.print(seed);
+  randomSeed(seed);
   FastLED.addLeds<WS2811,DATA_PIN,RGB>(leds,NUM_LEDS);
-  candlePalette = CRGBPalette16( CRGB::DarkOrange, CRGB::Orange, CRGB::DarkOrange, CRGB::SandyBrown);
+  // 0xFF4D00  // dark-red orange
+  // 0xFF5E00  // red-orange
+  // 0xFF7400  // darker orange
+  // 0xFF8C00  // dark orange (class)
+  // 0xFFA500  // orange (class)
+  // 0xFF9900  // light orange
+  // 0xFF9E0C  // light bright orange
+  candlePalette = CRGBPalette16( CRGB::DarkOrange, 0xFF7400, 0xFF9E0C, CRGB::Orange);
+
+  // 
   endingCounter = 50;
 }
  
@@ -495,6 +476,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   // serialInfo();
   handleMode();
-  EVERY_N_SECONDS(30) { endingCounter = 50; }
+  EVERY_N_SECONDS(35) { endingCounter = 50; }
   delay(20);
 }
